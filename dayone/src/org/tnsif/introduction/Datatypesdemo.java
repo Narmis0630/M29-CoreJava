b97751ecddee94f697d2b6ba1c3f4bac5afc5913
Simran Sankhe <Simran Sankhe@Navsacha-Laptop>
2023-04-05 00:03:59 +0530

    Second commit

--- dayone/src/org/tnsif/introduction/Datatypesdemo.java
+++ dayone/src/org/tnsif/introduction/Datatypesdemo.java
@@ -3,6 +3,16 @@ package org.tnsif.introduction;
 public class Datatypesdemo {
 
 	public static void main(String[] args) {
+		int num1=5/2;
+		float num2=102/45;
+		double num3= 16d/7d; //d will give precision upto 7 values//
+		float num4= 10f/3f; //f will give precicion upto 16 values//
+		System.out.println("value is" +num1);
+		System.out.println("value is" +num2);
+		System.out.println("value is" +num3);
+		System.out.println("value is" +num4);
+		
+		
 		// TODO Auto-generated method stub
 
 	}
