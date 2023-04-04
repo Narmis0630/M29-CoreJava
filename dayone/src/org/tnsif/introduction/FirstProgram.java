b2b88da942689e817a459a11db56fc7a37017a4f
Simran Sankhe <Simran Sankhe@Navsacha-Laptop>
2023-04-04 19:35:31 +0530

    first commit

---
+++ dayone/src/org/tnsif/introduction/FirstProgram.java
@@ -0,0 +1,11 @@
+package org.tnsif.introduction;
+
+public class FirstProgram {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		// TODO Auto-generated method stub
+
+	}
+
+}
